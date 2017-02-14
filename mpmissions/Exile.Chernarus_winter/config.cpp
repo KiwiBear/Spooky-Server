cbed40ed5c799f6a1d4db6d680e593cb85663e81
ranzor4063 <randygoetz1@Hotmail.com>
2017-02-13 17:53:55 -0800

    changed loot tables

--- mpmissions/Exile.Chernarus_winter/config.cpp
+++ mpmissions/Exile.Chernarus_winter/config.cpp
@@ -2801,7 +2801,7 @@ class CfgExileLootSettings
 	 *
 	 * You can also cap it at a maximum value. See below.
 	 */
-	maximumPositionCoverage = 30;
+	maximumPositionCoverage = 20;
 
  	/**
  	 * Limit the number of loot positions per building. If the 
@@ -2811,7 +2811,7 @@ class CfgExileLootSettings
  	 * This results in 30 loot positions and that is too much. So we
  	 * cap this at 10
  	 */
-	maximumNumberOfLootSpotsPerBuilding = 4;
+	maximumNumberOfLootSpotsPerBuilding = 3;
 
 	/**
 	 * Exile spawns a random number of items per loot spot. This 
@@ -3700,15 +3700,15 @@ class CfgTerritories
 	{
 		// Purchase Price 		Radius 		Number of Objects
 		{5000,					15,			30 					}, // Level 1
-		{10000,					30,			60 					}, // Level 2 
-		{15000,					45,			90 					}, // Level 3
-		{20000,					60,			120					}, // Level 4
-		{25000,					75,			150					}, // Level 5
-		{30000,					90,			180					}, // Level 6
-		{35000,					105,		210					}, // Level 7
-		{40000,					120,		240					}, // Level 8
-		{45000,					135,		270					}, // Level 9
-		{50000,					150,		300					}  // Level 10
+		{2000,					30,			40					}, // Level 2 
+		{3000,					45,			50 					}, // Level 3
+		{4000,					60,			60					}, // Level 4
+		{5000,					75,			70					}, // Level 5
+		{6000,					90,			80					}, // Level 6
+		{7000,					105,		90					}, // Level 7
+		{8000,					120,		100					}, // Level 8
+		{9000,					135,		130					}, // Level 9
+		{10000,					150,		150					}  // Level 10
 	};
 
 	// A shortcut of the above maximum radius
@@ -3724,16 +3724,16 @@ class CfgTerritories
 	 * Defines the minimum distance to safe zones / trader cities where players
 	 * cannot build territories
 	 */
-	minimumDistanceToTraderZones = 1000;
+	minimumDistanceToTraderZones = 1500;
 
 	/**
 	 * Defines the minimum distance to spawn zones where players
 	 * cannot build territories
 	 */
-	minimumDistanceToSpawnZones = 1000;
+	minimumDistanceToSpawnZones = 0;
 
 	// Amount of pop tabs per object to pay
-	popTabAmountPerObject = 10;
+	popTabAmountPerObject = 125;
 };
 class CfgTraderCategories 
 {
