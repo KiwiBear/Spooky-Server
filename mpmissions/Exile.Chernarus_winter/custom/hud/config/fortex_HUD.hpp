a86ab8854e6ca498057ef3702fdd6272ff17b4b6
TalksTooMuch <nic.opalski@gmail.com>
2017-02-15 23:33:20 -0800

    tweaked hud icon locations

--- mpmissions/Exile.Chernarus_winter/custom/hud/config/fortex_HUD.hpp
+++ mpmissions/Exile.Chernarus_winter/custom/hud/config/fortex_HUD.hpp
@@ -24,43 +24,102 @@ class Fortex_hud {
     fadeout = 0;
     text = "";
     class ControlsBackground {
-            class HEALTH_BKG: RscPicture {
+            
+			// temp icon
+			class TEMP_BKG: RscPicture {
+                idc = 1212;
+                text = "custom\hud\images\hud\status_bg.paa";
+                x = 0.95 * safezoneW + safezoneX;
+                y = 0.62 * safezoneH + safezoneY;
+                w = 0.0257812 * safezoneW;
+                h = 0.044 * safezoneH;
+            };
+            class TEMP: RscPicture {
+                idc = 1213;
+                text = "custom\hud\images\hud\status_temp_border_CA.paa";
+                x = 0.95 * safezoneW + safezoneX;
+                y = 0.62 * safezoneH + safezoneY;
+                w = 0.0257812 * safezoneW;
+                h = 0.044 * safezoneH;
+            };
+            class TEMP_CLR: RscPicture {
+                idc = 1214;
+                //text = "images\hud\status_temp_3_ca.paa";
+                x = 0.95 * safezoneW + safezoneX;
+                y = 0.62 * safezoneH + safezoneY;
+                w = 0.0257812 * safezoneW;
+                h = 0.044 * safezoneH;
+            };
+			
+			
+			// drink icon
+			class THIRST_BKG: RscPicture {
+                idc = 1209;
+                text = "custom\hud\images\hud\status_bg.paa";
+                x = 0.95 * safezoneW + safezoneX;
+                y = 0.69 * safezoneH + safezoneY;
+                w = 0.0257812 * safezoneW;
+                h = 0.044 * safezoneH;
+            };
+            class THIRST: RscPicture {
+                idc = 1210;
+                text = "custom\hud\images\hud\status_thirst_border_CA.paa";
+                x = 0.95 * safezoneW + safezoneX;
+                y = 0.69 * safezoneH + safezoneY;
+                w = 0.0257812 * safezoneW;
+                h = 0.044 * safezoneH;
+            };
+            class THIRST_CLR: RscPicture {
+                idc = 1211;
+                //text = "images\hud\status_thirst_inside_3_ca.paa";
+                x = 0.95 * safezoneW + safezoneX;
+                y = 0.69 * safezoneH + safezoneY;
+                w = 0.0257812 * safezoneW;
+                h = 0.044 * safezoneH;
+            };
+			
+			
+			// blood icon
+			class HEALTH_BKG: RscPicture {
                 idc = 1200;
                 text = "custom\hud\images\hud\status_bg.paa";
-                x = 0.971281 * safezoneW + safezoneX;
-                y = 0.896 * safezoneH + safezoneY;
+                x = 0.95 * safezoneW + safezoneX;
+                y = 0.76 * safezoneH + safezoneY;
                 w = 0.0257812 * safezoneW;
                 h = 0.044 * safezoneH;
             };
             class BLOOD: RscPicture {
                 idc = 1201;
                 text = "custom\hud\images\hud\status_blood_border_CA.paa";
-                x = 0.97025 * safezoneW + safezoneX;
-                y = 0.896 * safezoneH + safezoneY;
+                x = 0.95 * safezoneW + safezoneX;
+                y = 0.76 * safezoneH + safezoneY;
                 w = 0.0257812 * safezoneW;
                 h = 0.044 * safezoneH;
             };
             class BLOOD_CLR: RscPicture {
                 idc = 1210;
                 //text = "images\hud\status_blood_inside_3_ca.paa";
-                x = 0.97025 * safezoneW + safezoneX;
-                y = 0.896 * safezoneH + safezoneY;
+                x = 0.95 * safezoneW + safezoneX;
+                y = 0.76 * safezoneH + safezoneY;
                 w = 0.0257812 * safezoneW;
                 h = 0.044 * safezoneH;
             };
+			
+			
+			// food icon
             class FOOD_BKG: RscPicture {
                 idc = 1206;
                 text = "custom\hud\images\hud\status_bg.paa";
-                x = 0.969219 * safezoneW + safezoneX;
-                y = 0.8014 * safezoneH + safezoneY;
+                x = 0.95 * safezoneW + safezoneX;
+                y = 0.83 * safezoneH + safezoneY;
                 w = 0.0257812 * safezoneW;
                 h = 0.044 * safezoneH;
             };
             class FOOD: RscPicture {
                 idc = 1207;
                 text = "custom\hud\images\hud\status_food_border_CA.paa";
-                x = 0.97025 * safezoneW + safezoneX;
-                y = 0.8014 * safezoneH + safezoneY;
+                x = 0.95 * safezoneW + safezoneX;
+                y = 0.83 * safezoneH + safezoneY;
                 w = 0.0257812 * safezoneW;
                 h = 0.044 * safezoneH;
             };
@@ -68,56 +127,8 @@ class Fortex_hud {
                 idc = 1208;
 
                 //text = "images\hud\status_food_inside_2_ca.paa";
-                x = 0.97025 * safezoneW + safezoneX;
-                y = 0.8014 * safezoneH + safezoneY;
-                w = 0.0257812 * safezoneW;
-                h = 0.044 * safezoneH;
-            };
-            class THIRST_BKG: RscPicture {
-                idc = 1209;
-                text = "custom\hud\images\hud\status_bg.paa";
-                x = 0.97025 * safezoneW + safezoneX;
-                y = 0.8498 * safezoneH + safezoneY;
-                w = 0.0257812 * safezoneW;
-                h = 0.044 * safezoneH;
-            };
-            class THIRST: RscPicture {
-                idc = 1210;
-                text = "custom\hud\images\hud\status_thirst_border_CA.paa";
-                x = 0.97025 * safezoneW + safezoneX;
-                y = 0.8476 * safezoneH + safezoneY;
-                w = 0.0257812 * safezoneW;
-                h = 0.044 * safezoneH;
-            };
-            class THIRST_CLR: RscPicture {
-                idc = 1211;
-                //text = "images\hud\status_thirst_inside_3_ca.paa";
-                x = 0.97025 * safezoneW + safezoneX;
-                y = 0.8476 * safezoneH + safezoneY;
-                w = 0.0257812 * safezoneW;
-                h = 0.044 * safezoneH;
-            };
-            class TEMP_BKG: RscPicture {
-                idc = 1212;
-                text = "custom\hud\images\hud\status_bg.paa";
-                x = 0.97025 * safezoneW + safezoneX;
-                y = 0.7552 * safezoneH + safezoneY;
-                w = 0.0257812 * safezoneW;
-                h = 0.044 * safezoneH;
-            };
-            class TEMP: RscPicture {
-                idc = 1213;
-                text = "custom\hud\images\hud\status_temp_border_CA.paa";
-                x = 0.97025 * safezoneW + safezoneX;
-                y = 0.753 * safezoneH + safezoneY;
-                w = 0.0257812 * safezoneW;
-                h = 0.044 * safezoneH;
-            };
-            class TEMP_CLR: RscPicture {
-                idc = 1214;
-                //text = "images\hud\status_temp_3_ca.paa";
-                x = 0.97025 * safezoneW + safezoneX;
-                y = 0.753 * safezoneH + safezoneY;
+                x = 0.95 * safezoneW + safezoneX;
+                y = 0.83 * safezoneH + safezoneY;
                 w = 0.0257812 * safezoneW;
                 h = 0.044 * safezoneH;
             };
